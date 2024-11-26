34e2de2e7f84ad4d084d6ab2c773b7b777639310
cksdn <cksdngh@gmail.com>
2024-11-27 06:28:52 +0900

    20241127 pcw

--- yumyum_front/src/App.tsx
+++ yumyum_front/src/App.tsx
@@ -1,13 +1,5 @@
 import React from "react";
 import "./App.css";
-import Header from "./component/Header";
-import SideBar from "./component/SideBar";
-import Footer from "./component/Footer";
-import { Route, Routes } from "react-router-dom";
-import SignUp from "./views/Authentication/SignUp/SignUp";
-import { Box } from "@mui/material";
-import React from "react";
-import "./App.css";
 import { Route, Routes } from "react-router-dom";
 import { AUTH_PATH_SIGN_UP, REVIEW_PATH, STATS_MENU } from "./constants";
 import Review from "./views/Review/Review";
@@ -15,22 +7,10 @@ import MenusStats from "./views/Stats/MenusStats";
 import SignUp from "./views/Authentication/SignUp/SignUp";
 import Header from "./layouts/Header/index";
 import Footer from "./layouts/Footer";
-import SideBar from "./layouts/sideBar"
+import SideBar from "./layouts/sideBar";
 
 function App() {
   return (
-    <Box sx={{ display: "flex", flexDirection: "column", minHeight: "100vh" }}>
-      <Header />
-      <Box sx={{ display: "flex", flexGrow: 1, mt: "64px" }}>
-        <SideBar />
-        <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
-          <Routes>
-            <Route path="/signUp" element={<SignUp />} />
-          </Routes>
-        </Box>
-      </Box>
-      <Footer />
-    </Box>
     <div className="App">
       <Header />
       <SideBar/>
