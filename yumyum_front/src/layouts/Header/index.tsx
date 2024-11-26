ce6947c0183f53b7904fb659e0ce0e1212d32adb
cksdn <cksdngh@gmail.com>
2024-11-26 18:52:27 +0900

    20241126 pcw

--- yumyum_front/src/layouts/Header/index.tsx
+++ yumyum_front/src/layouts/Header/index.tsx
@@ -1,7 +1,27 @@
-import React from 'react'
+import * as React from "react";
+import Box from "@mui/material/Box";
+import CssBaseline from "@mui/material/CssBaseline";
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import { FormGroup } from "@mui/material";
 
-export default function index() {
+const drawerWidth = 240;
+
+export default function Header() {
   return (
-    <div>index</div>
-  )
+    <Box sx={{ display: "flex", marginBottom : "100px"}}>
+      {/* <CssBaseline /> */}
+      <AppBar
+        position="fixed"
+        sx={{ width: `calc(100% - ${drawerWidth}px)`, ml: `${drawerWidth}px`}}
+      >
+        <Toolbar>
+          <Typography variant="h6" noWrap component="div">
+            <FormGroup>header</FormGroup>
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      </Box>
+  );
 }
