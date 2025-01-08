11e30b17c4f336418d6629f3dcac77b6862392e7
cksdn <cksdngh@gmail.com>
2025-01-08 15:20:22 +0900

    20250108 pcw

---
+++ yumyum_front/src/views/Review/ReviewNotice.tsx
@@ -0,0 +1,107 @@
+/** @jsxImportSource @emotion/react */
+import React, {useEffect, useRef} from 'react';
+import * as css from "./Style";
+import ReactQuill from "react-quill-new";
+import moment from "moment/moment";
+import axios from "axios";
+import {useCookies} from "react-cookie";
+import QuillResizeImage from "quill-resize-image";
+import Quill from "quill";
+
+Quill.register('modules/resize', QuillResizeImage);
+
+const ReviewNotice = () => {
+    const [cookies] = useCookies(['token']);
+    const token = cookies.token;
+
+    const quillRef = useRef<ReactQuill | null>(null);
+
+    const handleSubmit = async () => {
+        if (!quillRef.current) return;
+        const editor = quillRef.current.getEditor();
+        const editorContentHTML = editor.root.innerHTML;
+        const base64ImageRegex = /<img[^>]+src="data:image\/[^">]+"[^>]*>/g;
+        const srcExtractRegex = /src="([^"]+)"/;
+
+        let newHTML = editorContentHTML;
+
+        const formData = new FormData();
+        const matches = editorContentHTML.match(base64ImageRegex) || [];
+        matches.forEach((imgTag, index) => {
+            const srcMatch = imgTag.match(srcExtractRegex);
+            if (!srcMatch) return;
+            const base64URL = srcMatch[1];
+            const file = dataURLtoFile(base64URL, `image_${index}.png`);
+
+            const newImgTag = imgTag.replace(base64URL, `image_${index}.png`);
+            newHTML = newHTML.replace(imgTag, newImgTag);
+            formData.append('noticePhotoUrl', file);
+        });
+
+        const noticeDate = moment().format('YYYY-MM-DDTHH:mm:ss');
+        formData.append('noticeDate', noticeDate);
+        formData.append('noticeText', newHTML);
+
+        try {
+            console.log('업로드할 FormData:', formData);
+
+            const response = await axios.post('http://localhost:4041/api/v1/reviews/notice/create', formData, {
+                headers: {
+                    Authorization: `Bearer ${token}`, 'Content-Type': 'multipart/form-data',
+                },
+            });
+
+            console.log('서버 응답:', response.data);
+
+            alert('공지사항 등록 완료');
+        } catch (error) {
+            console.error(error);
+            alert('업로드 중 문제가 발생했습니다.');
+        }
+    };
+
+
+    // Quill toolbar 설정
+    const toolbarOptions = [['image', 'link'], [{header: [1, 2, 3, false]}], ['bold', 'italic', 'underline', 'strike'], [{list: 'ordered'}, {list: 'bullet'}, {indent: '-1'}, {indent: '+1'}], [{align: []}, {color: []}, {background: []}], ['clean'],];
+
+    const modules = {
+        toolbar: toolbarOptions, resize: {},
+    };
+
+
+    const dataURLtoFile = (dataurl: string, filename: string) => {
+        const arr = dataurl.split(',');
+        const mime = arr[0].match(/:(.*?);/)?.[1] || 'image/png';
+        const bstr = atob(arr[1]);
+        let n = bstr.length;
+        const u8arr = new Uint8Array(n);
+        while (n--) {
+            u8arr[n] = bstr.charCodeAt(n);
+        }
+        return new File([u8arr], filename, {type: mime});
+    };
+
+
+    useEffect(() => {
+
+    }, []);
+
+    return (
+        <>
+            <div css={css.quillContainer}>
+                <ReactQuill
+                    ref={quillRef}
+                    css={css.quill}
+                    modules={modules}
+                    theme="snow"
+                    placeholder="리뷰 공지사항을 작성해주세요"
+                />
+            </div>
+            <div css={css.reviewNoticeUploadButtonContainer}>
+                <button css={css.reviewNoticeUploadButton} onClick={handleSubmit}>저장</button>
+            </div>
+        </>
+    );
+};
+
+export default ReviewNotice;
